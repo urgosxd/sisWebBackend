0c7785c059e8673342eda25cff0c9a026de57ded
chamba <urgosxd@gmail.com>
2024-01-20 11:36:05 -0800

    ga

--- crud/models.py
+++ crud/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Document(models.Models):
+class Document(models.Model):
     Extension = models.CharField(max_length=50, null=True)
     FileName = models.CharField(max_length=200, null=True)
     Doc_Content = models.BinaryField(null=True)
