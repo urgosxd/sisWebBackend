b5a4ef2a65372d19ad92b2cb92a60c4d99305f95
chamba <urgosxd@gmail.com>
2024-01-21 13:38:25 -0800

    aoe

--- drfauth/urls.py
+++ drfauth/urls.py
@@ -1,7 +1,8 @@
 from dj_rest_auth.views import AllowAny, LoginView
 from django.urls import path
-
+from dj_rest_auth.registration.views import RegisterView
 
 urlpatterns = [
         path("login/",LoginView.as_view(),name="rest_login"),
+         path("register/", RegisterView.as_view(), name="rest_register"),
         ]
