3daa381d6beddad58fc12e3841a1d26b90363e31
chamba <urgosxd@gmail.com>
2024-01-22 10:03:16 -0800

    gaa

--- core/settings.py
+++ core/settings.py
@@ -88,7 +88,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    'drfauth.backends.EmailBackend'
+    # 'drfauth.backends.EmailBackend'
 ]
 
 MIDDLEWARE = [
