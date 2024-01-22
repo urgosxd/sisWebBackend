d195fbc852d449e36d0600bd998c53dbdcbc0bd3
chamba <urgosxd@gmail.com>
2024-01-22 12:05:34 -0800

    mmo

--- drfauth/urls.py
+++ drfauth/urls.py
@@ -1,4 +1,5 @@
 from dj_rest_auth.views import AllowAny, LoginView
+from dj_rest_auth.jwt_auth import get_refresh_view
 from django.urls import path
 from dj_rest_auth.registration.views import RegisterView
 
@@ -7,4 +8,5 @@ from drfauth.views import CustomLoginEmailView
 urlpatterns = [
         path("login/",CustomLoginEmailView.as_view(),name="rest_login"),
          path("register/", RegisterView.as_view(), name="rest_register"),
+         path("tokken/refresh/",get_refresh_view,name="gaa")
         ]
