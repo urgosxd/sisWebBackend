788e12e1b8ab52575fc32be5b9ea40a2af2ec5eb
chamba <urgosxd@gmail.com>
2024-01-24 06:04:44 -0800

    gaa

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,4 @@
+from dj_rest_auth.views import IsAuthenticated
 from django.shortcuts import render
 from rest_framework import viewsets
 from crud.models import Tour
@@ -7,5 +8,15 @@ from crud.serializer import TourModelSerializer
 # Create your views here.
 
 class TourView(viewsets.ModelViewSet):
+    # permission_classes = []
     serializer_class = TourModelSerializer
     queryset = Tour.objects.all()
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'list':
+            permission_classes = []
+        else: 
+            permission_classes = [IsAuthenticated]
+        
+        return [permision() for permision in permission_classes]
+    
