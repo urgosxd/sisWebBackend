880d46a3e463b98b32c3fa52739b93263547d10e
chamba <urgosxd@gmail.com>
2024-01-30 14:33:55 -0800

    aoe

--- crud/serializer.py
+++ crud/serializer.py
@@ -1,8 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 from crud.models import FichaTecnica, Tour
 from rest_framework import serializers
+import base64
 
 class FichaTecnicaSerializer(ModelSerializer):
+    def create(self, validated_data):
+        print("GAA")
+        format, filestr = validated_data["Doc_Content"].split(';base64,')  # format ~= data:image/X,
+        ext = format.split('/')[-1]  # guess file extension
+        validated_data["Doc_Content"] = base64.b64decode(filestr)
+        instance = FichaTecnica.objects.create(**validated_data)
+        return instance
+    def validate_Doc_Content(self, value):
+        print(value)
+        return value
     class Meta:
         model = FichaTecnica
         fields='__all__'
--- crud/views.py
+++ crud/views.py
@@ -7,14 +7,16 @@ import tempfile
 import os
 from crud.serializer import FichaTecnicaSerializer, TourModelSerializer
 from rest_framework.response import Response
-
+from django.db import transaction 
 from django.contrib.staticfiles.storage import staticfiles_storage
+from rest_framework.parsers import MultiPartParser,FormParser,JSONParser
 # Create your views here.
 
 class TourView(viewsets.ModelViewSet):
     # permission_classes = []
     serializer_class = TourModelSerializer
     queryset = Tour.objects.all()
+    parser_classes = [MultiPartParser,FormParser,JSONParser]
     # def get_queryset(self):
     #     print(dir(self.request))
     #     return Tour.objects.all()
@@ -25,26 +27,26 @@ class TourView(viewsets.ModelViewSet):
         else: 
             permission_classes = []
         return [permision() for permision in permission_classes]
-    def perform_create(self, serializer):
-        tour = serializer.save()
-        print(self.request.parser_context['kwargs'])
-        # tour.fichasTecnicas.
+    def perform_create(self, serializer,files):
+        # print(files)
+        ga= FichaTecnicaSerializer(data=files,many=True)
+        if ga.is_valid():
+            ga.save()
+        else:
+            print(ga.errors)
+            # print("NOOO")
+
+        # with transaction.atomic():
+            # tour = serializer.save()
     def create(self, request, *args, **kwargs):
-        print(request.data)
+        # print(request.data)
         serializer = self.get_serializer(data = request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer,request.data["FileName"],request.data["Extension"],request.data["Doc_Content"])
+        self.perform_create(serializer,request.data["fichas"])
+        # print(serializer.errors)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    def perform_update(self, serializer,*args):
-        tour = serializer.save()
-        tour.fichasTecnicas.all().delete()
 
-        
-
-
-
-    
  # class FichaTecnicaView(viewsets.ModelViewSet):
     # permission_classes = []
 def write_file(data, filename,ext):
@@ -71,7 +73,7 @@ class FichaTecnicaView(viewsets.ModelViewSet):
         return [permision() for permision in permission_classes]
     def retrieve(self, request, *args,**kwargs):
         instance = self.get_object()
-        serializer = self.get_serializer(instance)
+        # serializer = self.get_serializer(instance)
         path = write_file(instance.Doc_Content,instance.FileName,instance.Extension)
         # file_path = staticfiles_storage.path(one.FileName+"."+one.Extension)
         with open(path,'rb') as pdf:
