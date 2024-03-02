e5762f79cb9c7189e51ab2ba9af167840368222d
chamba <urgosxd@gmail.com>
2024-03-02 13:17:02 -0800

    aoe

--- core/settings.py
+++ core/settings.py
@@ -148,7 +148,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(day=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False, 
     'BLACKLIST_AFTER_ROTATION': False, 
     # 'UPDATE_LAST_LOGIN': True,
