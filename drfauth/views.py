af03c8dfa18191404469e4edd8b7880f8e3c9299
chamba <urgosxd@gmail.com>
2024-03-02 13:31:36 -0800

    aoe

--- drfauth/views.py
+++ drfauth/views.py
@@ -2,7 +2,7 @@
 from dj_rest_auth.registration.views import RegisterView
 from django.shortcuts import render
 from dj_rest_auth.views import AllowAny, LoginView
-from drfauth.serializer import CustomLoginSerializer
+# from drfauth.serializer import CustomLoginSerializer
 
 
 
