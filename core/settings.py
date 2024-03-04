7411d85073dbd3d5b281ea334e96391dbcd31d29
user <020200295a@uandina.edu.pe>
2024-03-03 22:45:12 -0800

    aoe

--- core/settings.py
+++ core/settings.py
@@ -149,7 +149,7 @@ ROOT_URLCONF = 'core.urls'
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True, 
     'BLACKLIST_AFTER_ROTATION': True, 
